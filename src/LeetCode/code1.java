980e49df07e5f5659e4e03469e22241794b21d6a
MHGCC <3202290650@qq.com>
2022-05-30 11:30:24 +0800

    Java code first commit

---
+++ src/LeetCode/code1.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class code1 {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] results=new int[2];
+        Map<Integer,Integer> int_nums=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(int_nums.containsKey(target-nums[i])){
+                results[0]=i;
+                results[1]=int_nums.get(target-nums[i]);
+            }
+            else{
+                int_nums.put(nums[i],i);
+            }
+        }
+        return results; 
+    }
+    public static void main(String[] args){
+        int[] nums={2,7,11,15};
+        int target=9;
+        System.out.print(Arrays.toString(twoSum(nums,target)));
+    }
+}
